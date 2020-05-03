264d96774243f6bd62607f968c46bd7d2a994c2c
kota ishizuka <kota.ishizuka1210@gmail.com>
2020-05-03 16:45:54 +0900

    use component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <lable htmlFor="foo">foo</lable>
-        <input type="text" onChange={() => {console.log("I am clicked!")}}/>
-      </React.Fragment>
-    )
-  }
+// クラスComponent
+// class App extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <lable htmlFor="foo">foo</lable>
+//         <input type="text" onChange={() => {console.log("I am clicked!")}}/>
+//       </React.Fragment>
+//     )
+//   }
+// }
+
+// 関数Component
+const App = () => {
+  return (
+    <React.Fragment>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </React.Fragment>
+  )
+}
+
+const Cat = () => {
+  return <div>Meow!</div>
 }
 
 export default App;
